ae01602c5115994e94d4ff07d0278bb015ef2e0d
Jacob Hansen <jacob@econify.com>
2022-11-20 00:16:46 -0600

    Adds second button to delete page

--- src/pages/delete-all-posts/index.js
+++ src/pages/delete-all-posts/index.js
@@ -7,9 +7,11 @@ export default function DeleteAllPosts() {
   return (
     <Flex justify="center" align="center" direction="column">
       <Template />
+
       <Text fontSize={24} marginTop={125} marginBottom={10} fontWeight="bold">
         Delete All Posts?
       </Text>
+
       <NextLink href="/home" passHref>
         <Link
           _hover={{
@@ -17,6 +19,18 @@ export default function DeleteAllPosts() {
           }}
         >
           <Button onClick={deleteAllPosts}>Delete All 🗑️</Button>
+        </Link>
+      </NextLink>
+
+      <NextLink href="/home" passHref>
+        <Link
+          _hover={{
+            textDecoration: "none",
+          }}
+        >
+          <Button onClick={deleteAllPosts} mt={10}>
+            Go Back 😳
+          </Button>
         </Link>
       </NextLink>
     </Flex>
