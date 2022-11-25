f29fb926beac916d1eb382bc47c8966de321cee5
Jacob Hansen <jacob@econify.com>
2022-11-24 23:18:51 -0600

    adds another service worker error handler

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -22,10 +22,18 @@ self.addEventListener("fetch", (event) => {
         // Even if the response is in the cache, we fetch it
         // and update the cache for future usage
         const fetchPromise = fetch(event.request).then((networkResponse) => {
-          caches.open("assets").then((cache) => {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          });
+          caches
+            .open("assets")
+            .then((cache) => {
+              cache.put(event.request, networkResponse.clone());
+              return networkResponse;
+            })
+            .catch((error) => {
+              console.log(
+                "Error saving fetch response to cache in service worker. ",
+                error
+              );
+            });
         });
         // We use the currently cached version if it's there
         return cachedResponse || fetchPromise; // cached or a network fetch
