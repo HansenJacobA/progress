b2838c7b82467b2b7606bce8d7a2058be854df62
Jacob Hansen <jacob@econify.com>
2022-11-26 15:22:01 -0600

    refactors eslint json

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Script src="/sw-register.js" />
+      {/* <Script src="/sw-register.js" /> */}
       <Component {...pageProps} />
     </ChakraProvider>
   );
