32dc5c2df5bca180f0f14af4e50dd997471c6a49
Jacob Hansen <jacob@econify.com>
2022-11-26 17:00:01 -0600

    adds margin to bottom of post view

--- src/components/post/index.tsx
+++ src/components/post/index.tsx
@@ -7,7 +7,7 @@ import { details } from "../../utilities/postContent";
 export default function Post({ post }: { post: Post }) {
   const postDetails = details(post);
   return (
-    <Flex w="100%" direction="column" justify="center">
+    <Flex w="100%" direction="column" justify="center" mb={10}>
       {postDetails.map((detail, index) => {
         const subject = subjects[index];
         return (
